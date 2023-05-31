7e82e23501934acc577b00a80caf7b4891277cf8
Lucas Angelino <angelino.n.lucas@gmail.com>
2023-05-31 20:32:30 -0300

    frontend/add home recetas list

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -4,24 +4,38 @@ import {View, Text, StyleSheet} from 'react-native';
 const Navbar = ({navigation}) => {
   return (
     <View style={styles.nabvar}>
-      <View>
-        <Text style={styles.logo}>Cocinando</Text>
-      </View>
+      <Text style={styles.menu}>Menu</Text>
+      <Text style={styles.logo}>Cocinando</Text>
+      <Text style={styles.avatar}></Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   nabvar: {
-    flex: 1,
+    display: 'flex',
+    flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     backgroundColor: '#6D8C00',
-    paddingVertical: 20,
     height: 100,
+    borderBottomRightRadius: 20,
+    borderBottomLeftRadius: 20,
+    paddingHorizontal: 20,
+  },
+  menu: {
+    fontSize: 20,
+    color: '#fff',
   },
   logo: {
-    fontSize: 50,
+    fontSize: 20,
+    color: '#fff',
+  },
+  avatar: {
+    height: 50,
+    width: 50,
+    borderRadius: 100,
+    backgroundColor: '#fff',
     color: '#fff',
   },
 });
--- frontend/screens/Home.jsx
+++ frontend/screens/Home.jsx
@@ -1,21 +1,132 @@
 import React from 'react';
-import {View, TextInput, Text, StyleSheet} from 'react-native';
+import {
+  View,
+  TextInput,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+} from 'react-native';
+import Navbar from '../components/Navbar';
+
+const recetas = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+  {
+    id: 'bd7acbea-c1b1-46c2-dfdf-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-234-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-cgh-145571e29d72',
+    title: 'Third Item',
+  },
+];
 
 const Home = ({navigation}) => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.logo}>HOME</Text>
-      <View style={styles.form}>
-        <TextInput style={styles.input} placeholder="Email" />
+    <View style={styles.AppContainer}>
+      <Navbar />
+      <View style={styles.container}>
+        <View style={styles.inputSearch}>
+          <TextInput
+            style={styles.input}
+            placeholder="Nombre, ingredientes..."
+          />
+          <TouchableOpacity style={styles.searchBtn}>
+            <Text style={styles.searchText}>Buscar</Text>
+          </TouchableOpacity>
+        </View>
+        <Text style={styles.title}>Nuevas Recetas</Text>
+        <View style={styles.recetasContainer}>
+          <FlatList
+            style={styles.recetasList}
+            data={recetas}
+            renderItem={({item, index}) => (
+              <RecetaCard key={index} receta={item} />
+            )}
+            keyExtractor={item => item.id}
+          />
+        </View>
       </View>
     </View>
   );
 };
 
+const RecetaCard = ({receta}) => {
+  return (
+    <View style={styles.recetaCard}>
+      <Text>{receta.title}</Text>
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
-  container: {
+  AppContainer: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: '#fff',
+  },
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  recetasList: {
+    width: '100%',
+  },
+  inputSearch: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '80%',
+    backgroundColor: 'gray',
+    borderRadius: 100,
+    paddingHorizontal: 10,
+    marginVertical: 20,
+    marginTop: 150,
+  },
+  recetaCard: {
+    width: '100%',
+    height: 200,
+    backgroundColor: 'gray',
+    borderRadius: 10,
+    padding: 10,
+    marginVertical: 10,
+  },
+  title: {
+    fontSize: 25,
+    color: '#6D8C00',
+    marginBottom: 20,
+  },
+  recetasContainer: {
+    width: '80%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  searchBtn: {
+    backgroundColor: '#6D8C00',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 100,
+  },
+  searchText: {
+    color: '#000',
+    fontWeight: 'bold',
   },
 });
 
